377f623d4dea8d986d1146ffb9c772c064e4822d
jordisauras <jordi.sauras@e-campus.uab.cat>
2019-05-16 20:33:45 +0200

    Add files via upload
    Hope to hear from you soon. Thanks for the chance Gameloft.

---
+++ gameloftTest.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+
+class Card{
+public:
+    std::string Name;
+    std::string image;
+    int damage;
+    int type; // each type will be represented with a number
+    int element;////each element will be represented with a number
+    bool isHealing;
+    int heal;
+private:
+    int numberOfCopies;
+};
+
+class Character{
+    int uniqueIdentifier;
+    std::string name;
+    int* typeOfCardsThatCanUseMe[]; //array whith the type of cards that can be used for this character
+
+};
+
+class Deck{
+    Card* deck[]; //array of cards
+
+};
+
+class Player{
+    std::string name;
+    int uniqueIdentifier;
+    Card* allMyCards[]; //array with cards
+    Deck deck; // the player deck
+    Character character; //the player chosen Character
+};
